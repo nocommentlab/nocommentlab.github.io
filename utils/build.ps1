72c933cff7e65f7252a572c93d2e0ee857594675
nocommentlab <blescia.antonio@gmail.com>
2020-03-15 11:39:52 +0100

    Powershell scripts commented

--- utils/build.ps1
+++ utils/build.ps1
@@ -1,7 +1,26 @@
-# Builds index.html
-pandoc ..\index.md -s -f gfm -t html5 --template=..\template\index.html --defaults=..\defaults\params_index.yaml --output ..\index.html
+function New-RebuildBlog{
+  <#
+    .SYNOPSIS
+        Builds all website pages
 
-$md_files = Get-ChildItem ..\articles\ | Where-Object {$_.Extension -eq ".md"}
-$md_files | ForEach-Object{
-	pandoc ..\articles\$_ -s -f gfm -t html5 --template=..\template\article.html --defaults=..\defaults\params_$($_.BaseName).yaml --output ..\articles\$($_.BaseName).html
+    .DESCRIPTION
+        This function builds for fist the index.md page, after, builds all *.md files inside the articles page
+
+    .OUTPUTS
+        All generated html files
+
+    .EXAMPLE
+        New-Article
+    #>
+
+	# Builds index.html
+	pandoc ..\index.md -s -f gfm -t html5 --template=..\template\index.html --defaults=..\defaults\params_index.yaml --output ..\index.html
+
+	# Gets all *.md files inside the articles folder
+	$md_files = Get-ChildItem ..\articles\ | Where-Object {$_.Extension -eq ".md"}
+	# Iterates over the *.md file list
+	$md_files | ForEach-Object{
+		# Using pandoc, generates the html files
+		pandoc ..\articles\$_ -s -f gfm -t html5 --template=..\template\article.html --defaults=..\defaults\params_$($_.BaseName).yaml --output ..\articles\$($_.BaseName).html
+	}
 }
